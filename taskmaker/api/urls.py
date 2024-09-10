8016f85565f89f8fd7e39260d1735d52e24a8f8a
raul crespo <raulxcrespo@gmail.com>
2024-09-10 15:55:52 -0300

    expose endpoint to date range search

--- taskmaker/api/urls.py
+++ taskmaker/api/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path("api/", views.TarefaListCreate.as_view(), name="tarefa-view-list-create"),
+    re_path(
+        "api/(?P<start_date>\d{4}-\d{2}-\d{2})/(?P<end_date>\d{4}-\d{2}-\d{2})$",
+        views.TarefaList.as_view(),
+        name="tarefa-view-list",
+    ),
     path(
         "api/<int:pk>/",
         views.TarefaRetrieveUpdateDestroy.as_view(),
