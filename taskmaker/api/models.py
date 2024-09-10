bfefb3eeae9ea8b6c15f6f448d2c6198aabe069f
raul crespo <raulxcrespo@gmail.com>
2024-09-10 13:19:17 -0300

    create Tarefa model
    Tarefa model with fields titulo and data (required), descricao and horario (optional)

--- taskmaker/api/models.py
+++ taskmaker/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Tarefa(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(null=True, blank=True)
+    data = models.DateField()
+    horario = models.TimeField(null=True, blank=True)
+
+    def __str__(self):
+        return (
+            "Título: "
+            + self.titulo
+            + "\nDescrição: "
+            + self.descricao
+            + "\nData: "
+            + self.data
+            + "\nHorário: "
+            + self.horario
+        )
