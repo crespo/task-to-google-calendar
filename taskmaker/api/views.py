d5859f9d302db1c0493bef7b6de4b3c6e992dd3d
raul crespo <raulxcrespo@gmail.com>
2024-09-10 15:35:16 -0300

    feature search query
    /api?search=test now searches for partial or full titles

--- taskmaker/api/views.py
+++ taskmaker/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics, status
+from rest_framework import generics, filters
 from .models import Tarefa
 from .serializers import TarefaSerializer
 
@@ -7,6 +7,8 @@ from .serializers import TarefaSerializer
 class TarefaListCreate(generics.ListCreateAPIView):
     queryset = Tarefa.objects.all()
     serializer_class = TarefaSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["titulo"]
 
 
 class TarefaRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
