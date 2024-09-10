e109787b33ea6323c4413d74f7faec386d149008
raul crespo <raulxcrespo@gmail.com>
2024-09-10 15:56:10 -0300

    make view for filter by date range

--- taskmaker/api/views.py
+++ taskmaker/api/views.py
@@ -4,6 +4,16 @@ from .models import Tarefa
 from .serializers import TarefaSerializer
 
 
+class TarefaList(generics.ListAPIView):
+    serializer_class = TarefaSerializer
+
+    def get_queryset(self):
+        start_date = self.kwargs.get("start_date")
+        end_date = self.kwargs.get("end_date")
+
+        return Tarefa.objects.filter(data__range=[start_date, end_date])
+
+
 class TarefaListCreate(generics.ListCreateAPIView):
     queryset = Tarefa.objects.all()
     serializer_class = TarefaSerializer
