91e0f4176866462420c1967d3fee8dea12b3829c
raul crespo <raulxcrespo@gmail.com>
2024-09-10 15:16:04 -0300

    make retrieveupdatedestroy view
    add api/<int:pk>/ endpoint and create RetrieveUpdateDestroy generic view

--- taskmaker/api/urls.py
+++ taskmaker/api/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("api/", views.TarefasListCreate.as_view(), name="tarefa-view-create"),
+    path("api/", views.TarefaListCreate.as_view(), name="tarefa-view-list-create"),
+    path(
+        "api/<int:pk>/",
+        views.TarefaRetrieveUpdateDestroy.as_view(),
+        name="tarefa-view-retrieve-update-destroy",
+    ),
 ]
--- taskmaker/api/views.py
+++ taskmaker/api/views.py
@@ -4,6 +4,12 @@ from .models import Tarefa
 from .serializers import TarefaSerializer
 
 
-class TarefasListCreate(generics.ListCreateAPIView):
+class TarefaListCreate(generics.ListCreateAPIView):
     queryset = Tarefa.objects.all()
     serializer_class = TarefaSerializer
+
+
+class TarefaRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Tarefa.objects.all()
+    serializer_class = TarefaSerializer
+    lookup_field = "pk"
