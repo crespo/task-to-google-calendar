6673e268a70edb9cfc3c895b5bfdf324f0b8b89c
raul crespo <raulxcrespo@gmail.com>
2024-09-12 13:36:35 -0300

    feat: rewrite retrieve, update and destroy logics to integrate with google api

--- taskmaker/api/views.py
+++ taskmaker/api/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from rest_framework import generics, filters, status
+from django.http import Http404
+from rest_framework import generics, status
 from .models import Tarefa
 from .serializers import TarefaSerializer
 from rest_framework.views import APIView
@@ -54,7 +54,7 @@ class TarefaView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class TarefaByDateRangeList(generics.ListAPIView):
+class TarefaByDateRangeListView(generics.ListAPIView):
     serializer_class = TarefaSerializer
 
     def get_queryset(self):
@@ -64,7 +64,36 @@ class TarefaByDateRangeList(generics.ListAPIView):
         return Tarefa.objects.filter(data__range=[start_date, end_date])
 
 
-class TarefaRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+class TarefaRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Tarefa.objects.all()
     serializer_class = TarefaSerializer
     lookup_field = "pk"
+
+    def get_object(self, pk):
+        try:
+            return Tarefa.objects.get(pk=pk)
+        except Tarefa.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        tarefa = self.get_object(pk)
+        serializer = TarefaSerializer(tarefa)
+
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        tarefa = self.get_object(pk)
+        serializer = TarefaSerializer(tarefa, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        tarefa = self.get_object(pk)
+        TaskManager.deleteTask(tarefa.task_id)
+        tarefa.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
