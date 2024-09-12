3dc6999d43bd5231f621fd6bfd7b16dfb07afd0c
raul crespo <raulxcrespo@gmail.com>
2024-09-11 23:37:29 -0300

    feat: service to connect with google api
    the services does the job of connecting and making the creation and deletion of tasks in google services

---
+++ taskmaker/api/services/TaskManager.py
@@ -0,0 +1,85 @@
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+SCOPES = ["https://www.googleapis.com/auth/tasks"]
+
+
+class TaskManager:
+    def authenticate():
+        creds = None
+        if os.path.exists("token.json"):
+            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "credentials.json", SCOPES
+                )
+                creds = flow.run_local_server(port=0)
+            with open("token.json", "w") as token:
+                token.write(creds.to_json())
+
+        return creds
+
+    def deleteTask(taskId):
+        creds = TaskManager.authenticate()
+
+        try:
+            service = build("tasks", "v1", credentials=creds)
+
+            service.tasks().delete(
+                tasklist=TaskManager.getFirstTasklistID(), task=taskId
+            ).execute()
+
+        except HttpError as err:
+            return err
+
+    def createTask(task):
+        creds = TaskManager.authenticate()
+
+        try:
+            service = build("tasks", "v1", credentials=creds)
+
+            response = (
+                service.tasks()
+                .insert(
+                    tasklist=TaskManager.getFirstTasklistID(creds),
+                    body={
+                        "title": task.titulo,
+                        "notes": task.descricao,
+                        "due": task.data + "T00:00:00.00Z",
+                    },
+                )
+                .execute()
+            )
+
+            return response
+
+        except HttpError as err:
+            return err
+
+    def getFirstTasklistID(creds):
+        try:
+            service = build("tasks", "v1", credentials=creds)
+
+            results = service.tasklists().list(maxResults=10).execute()
+            tasklists = results.get("items", [])
+
+            return tasklists[0]["id"]
+
+        except HttpError as err:
+            return err
+
+
+class Task:
+    def __init__(self, titulo, data, descricao=None, horario=None):
+        self.titulo = titulo
+        self.data = data
+        self.descricao = descricao
+        self.horario = horario
