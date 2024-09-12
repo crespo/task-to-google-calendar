f00cfe35c778d0566eabfbe19d36c9ddd74b3921
raul crespo <raulxcrespo@gmail.com>
2024-09-11 23:36:18 -0300

    feat: new  view
    rewrote the view for creating and retrieving tasks for google api support

--- taskmaker/api/urls.py
+++ taskmaker/api/urls.py
@@ -2,11 +2,11 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path("api/", views.TarefaListCreate.as_view(), name="tarefa-view-list-create"),
+    path("api/", views.TarefaView.as_view(), name="tarefa-view-list-create"),
     re_path(
         "api/(?P<start_date>\d{4}-\d{2}-\d{2})/(?P<end_date>\d{4}-\d{2}-\d{2})$",
-        views.TarefaList.as_view(),
-        name="tarefa-view-list",
+        views.TarefaByDateRangeList.as_view(),
+        name="tarefa-by-date-range-view-list",
     ),
     path(
         "api/<int:pk>/",
--- taskmaker/api/views.py
+++ taskmaker/api/views.py
@@ -1,10 +1,60 @@
 from django.shortcuts import render
-from rest_framework import generics, filters
+from rest_framework import generics, filters, status
 from .models import Tarefa
 from .serializers import TarefaSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .services.TaskManager import TaskManager, Task
+from googleapiclient.errors import HttpError
 
 
-class TarefaList(generics.ListAPIView):
+class TarefaView(APIView):
+    def get(self, request, format=None):
+        search = request.query_params.get("search", "")
+
+        if search:
+            tarefas = Tarefa.objects.filter(titulo__icontains=search)
+        else:
+            tarefas = Tarefa.objects.all()
+
+        serializer = TarefaSerializer(tarefas, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):
+        task = ""
+        request_data = request.data
+        errorOccurred = False
+
+        if "titulo" in request_data and "data" in request_data:
+            task = Task(
+                request_data["titulo"],
+                request_data["data"],
+            )
+
+            if "descricao" in request_data:
+                task.descricao = request_data["descricao"]
+
+            if "horario" in request_data:
+                task.horario = request_data["horario"]
+
+            createTaskResponse = TaskManager.createTask(task)
+
+            if type(createTaskResponse) is HttpError:
+                errorOccurred = True
+            else:
+                request_data["task_id"] = createTaskResponse["id"]
+
+        serializer = TarefaSerializer(data=request_data)
+
+        if serializer.is_valid():
+            if not errorOccurred:
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TarefaByDateRangeList(generics.ListAPIView):
     serializer_class = TarefaSerializer
 
     def get_queryset(self):
@@ -12,13 +62,6 @@ class TarefaList(generics.ListAPIView):
         end_date = self.kwargs.get("end_date")
 
         return Tarefa.objects.filter(data__range=[start_date, end_date])
-
-
-class TarefaListCreate(generics.ListCreateAPIView):
-    queryset = Tarefa.objects.all()
-    serializer_class = TarefaSerializer
-    filter_backends = [filters.SearchFilter]
-    search_fields = ["titulo"]
 
 
 class TarefaRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
