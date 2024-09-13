1635796ed3f5cd127899d7f67d68ee8431389851
raul crespo <raulxcrespo@gmail.com>
2024-09-13 15:51:42 -0300

    feat: create EventSerializer

--- taskmaker/api/serializers.py
+++ taskmaker/api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Task, Event
 
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ["id", "title", "notes", "date", "time", "task_id"]
+
+
+class EventSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Event
+        fields = ["id", "summary", "description", "date", "time", "event_id"]
