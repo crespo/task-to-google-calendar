1367074df35c339d4482167028e7d744529b76b1
raul crespo <raulxcrespo@gmail.com>
2024-09-13 16:18:32 -0300

    refactor: add version of the api to url path

--- taskmaker/api/urls.py
+++ taskmaker/api/urls.py
@@ -2,25 +2,25 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path("api/tasks/", views.TaskView.as_view(), name="task-list-create-view"),
+    path("api/v1/tasks/", views.TaskView.as_view(), name="task-list-create-view"),
     re_path(
-        r"api/tasks/(?P<start_date>\d{4}-\d{2}-\d{2})/(?P<end_date>\d{4}-\d{2}-\d{2})/$",
+        r"api/v1/tasks/(?P<start_date>\d{4}-\d{2}-\d{2})/(?P<end_date>\d{4}-\d{2}-\d{2})/$",
         views.TaskByDateRangeListView.as_view(),
         name="task-by-date-range-list-view",
     ),
     path(
-        "api/tasks/<int:pk>/",
+        "api/v1/tasks/<int:pk>/",
         views.TaskRetrieveUpdateDestroyView.as_view(),
         name="task-retrieve-update-destroy-view",
     ),
-    path("api/events/", views.EventView.as_view(), name="event-list-create-view"),
+    path("api/v1/events/", views.EventView.as_view(), name="event-list-create-view"),
     re_path(
-        r"api/events/(?P<start_date>\d{4}-\d{2}-\d{2})/(?P<end_date>\d{4}-\d{2}-\d{2})/$",
+        r"api/v1/events/(?P<start_date>\d{4}-\d{2}-\d{2})/(?P<end_date>\d{4}-\d{2}-\d{2})/$",
         views.EventByDateRangeListView.as_view(),
         name="event-by-date-range-list-view",
     ),
     path(
-        "api/events/<int:pk>/",
+        "api/v1/events/<int:pk>/",
         views.EventRetrieveUpdateDestroyView.as_view(),
         name="event-retrieve-update-destroy-view",
     ),
